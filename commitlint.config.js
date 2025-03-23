be3d792542d8e8a6b7f571b926297e5f94f74419
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2025-03-23 16:39:16 +0530

    feat(commitlint): add custom rules for line length enforcement
    - Increased maximum line length for commit body to 200 characters.
    - Increased maximum header length to 100 characters for better commit message formatting.

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,14 @@ const Configuration = {
    * Referenced packages must be installed
    */
   extends: ["@commitlint/config-conventional"],
+
+  // Add custom rules
+  rules: {
+    // Increase max line length (default is 100)
+    "body-max-line-length": [2, "always", 200],
+    // Increase header length (default is 100)
+    "header-max-length": [2, "always", 100],
+  },
 };
 
 module.exports = Configuration;
